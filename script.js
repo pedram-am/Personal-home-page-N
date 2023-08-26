ff1e7b069610cd45158cbafb35f6a415fbfbf943
pedram-am <127978419+pedram-am@users.noreply.github.com>
2023-08-26 15:01:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,151 +1,17 @@
-/* =================
-  TESTS, LOOK AT THESE
-  Reading tests will always help you discover your requirements.
-  You can make this window bigger. 
-   ===================
-*/
+var fruits, emojis;
 
-const {
-  core: { test, expect, run },
-  prettify
-} = window.jestLite;
 
-/* =================
-  FIND ELEMENTS
-  These are all the elements we will look for.
-   ===================
-*/
-const getHeader = document.querySelectorAll("header"),
-  getH1 = document.querySelectorAll("h1"),
-  getSiteHeader = document.querySelectorAll(".c-site-header"),
-  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
-  getMain = document.querySelectorAll("main"),
-  getFooter = document.querySelectorAll("footer"),
-  getSiteFooter = document.querySelectorAll(".c-site-footer"),
-  getIFrame = document.querySelectorAll("iframe"),
-  getImage = document.querySelectorAll("img"),
-  getWords = document.body.innerText;
+fruits = ['apple', 'banana', 'orange'];
+emojis = ['🍎', '🍌', '🍒'];
+let element_list = document.getElementById('list');
+while (!!fruits.length) {
+  if(--window.LoopTrap <= 0) throw "Infinite loop.";
+  let new_li = document.createElement('li');
+  new_li.innerText = fruits.shift();
+  let new_span = document.createElement('span');
+  new_span.innerText = emojis.shift();
 
-/* =================
-   ASSERTIONS 
-   These are the things we check are true about your page.
-   Read and update your HTML to discover the requirements.
-   The tests will run every time you update your code.
-   ===================
-*/
-test("There is at least one header element", () => {
-  expect(getHeader.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one h1", () => {
-  expect(getH1.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one header element with the class c-site-header", () => {
-  expect(getSiteHeader.length).toBe(1);
-});
-test("There is a nav element with an aria-label of Main Site Links.", () => {
-  expect(getAria.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one main element", () => {
-  expect(getMain.length).toBe(1);
-});
-test("There is at least one footer element", () => {
-  expect(getFooter.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one footer element with the class c-site-footer", () => {
-  expect(getSiteFooter.length).toBe(1);
-});
-test("There is embedded video", () => {
-  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one image", () => {
-  expect(getImage.length).toBeGreaterThanOrEqual(1);
-});
-test("There are at least 500 words on the page", () => {
-  expect(getWords.length).toBeGreaterThanOrEqual(500);
-});
+  new_li.appendChild(new_span);
 
-const console = document.getElementById("tests");
-prettify.toHTML(run(), console);
-
-function getWords(type) {
-  // Return words of a given type, or all words if type is 'WORD'
-  let words = [
-    {type: 'ADJECTIVE', value: 'big'},
-    {type: 'ADJECTIVE', value: 'purple'},
-    {type: 'ADJECTIVE', value: 'new'},
-    {type: 'ADJECTIVE', value: 'interesting'},
-    {type: 'ADJECTIVE', value: 'curious'},
-    {type: 'ADJECTIVE', value: 'happy'},
-    {type: 'ADJECTIVE', value: 'busy'},
-    {type: 'ADJECTIVE', value: 'tiny'},
-    {type: 'NOUN', value: 'umbrella'},
-    {type: 'NOUN', value: 'knee'},
-    {type: 'NOUN', value: 'banana'},
-    {type: 'NOUN', value: 'platypus'},
-    {type: 'NOUN', value: 'cat'},
-    {type: 'NOUN', value: 'mouse'},
-    {type: 'NOUN', value: 'house'},
-    {type: 'VERB', value: 'impressed'},
-    {type: 'VERB', value: 'honoured'},
-    {type: 'VERB', value: 'saw'},
-    {type: 'VERB', value: 'ate'},
-    {type: 'VERB', value: 'surprised'},
-    {type: 'VERB', value: 'annoyed'},
-    {type: 'VERB', value: 'touched'},
-    {type: 'VERB', value: 'understood'},
-    {type: 'VERB', value: 'taught'},
-  ];
-  return words.filter(word => type === 'WORD' || word.type === type).map(word => word.value);
+  element_list.appendChild(new_li);
 }
-
-function randomInt(n) {
-  // Return a random number from in [0, n[
-  return Math.floor(Math.random()*n);
-}
-
-function randomMember(arr) {
-  // Return a random member of the array
-  return arr[randomInt(arr.length)]
-}
-
-
-
-document.getElementById('noun1').addEventListener('click', (event) => {
-  event.target.innerText = randomMember(getWords('NOUN'));
-
-});
-
-document.getElementById('verb').addEventListener('click', (event) => {
-  event.target.innerText = randomMember(getWords('VERB'));
-
-});
-
-document.getElementById('adjective').addEventListener('click', (event) => {
-  event.target.innerText = randomMember(getWords('ADJECTIVE'));
-
-});
-
-document.getElementById('noun2').addEventListener('click', (event) => {
-  event.target.innerText = randomMember(getWords('NOUN'));
-
-});
-
-document.getElementById('daymode').addEventListener('click', (event) => {
-  let element_list = document.getElementById('list');
-  element_list.style.backgroundColor = '#ffffff';
-  element_list.style.color = '#000000';
-
-});
-
-document.getElementById('nightmode').addEventListener('click', (event) => {
-  let element_list2 = document.getElementById('list');
-  element_list2.style.backgroundColor = '#000000';
-  element_list2.style.color = '#ffffff';
-
-});
-
-document.getElementById('colour').addEventListener('click', (event) => {
-  event.target.style.color = '#ff0000';
-
-});
-randomMember(getWords('WORD'));
